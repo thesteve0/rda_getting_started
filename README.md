ff5d7659c116869e3d255c6c487b142d843aad3d
Steven Pousty <steve.pousty@gmail.com>
2018-12-05 12:19:25 -0800

    Makes HTML for the operators

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rda_getting_started
 My beginnings of using the REST API for RDA
+
+To make the operation JSON to tables we are trying this package
+
+https://pypi.org/project/json2table/
+
+conda install --channel "conda-forge" json2table
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from json2table import convert
 import requests
 from gbdx_auth import gbdx_auth
 
@@ -22,13 +23,21 @@ s = gbdx_auth.get_session()
 parameters = {'token': s.access_token}
 
 #make our request
-resp = requests.get('https://rda.geobigdata.io/v1/stripMetadata/4b077271-648e-47fc-a3aa-e01faac7eef1-inv', params=parameters)
+#resp = requests.get('https://rda.geobigdata.io/v1/stripMetadata/4b077271-648e-47fc-a3aa-e01faac7eef1-inv', params=parameters)
 
+#This is now for working on Operators
 operator_resp = requests.get("https://rda.geobigdata.io/v1/operator?fullListing=true", params=parameters)
 
 operator_json = json.loads(operator_resp.content)
+operator_json_dict = {"operators": operator_json}
+build_direction = "LEFT_TO_RIGHT"
+table_attributes = {"style" : "width:100%", "border": "1px solid black" }
 
+html = convert(operator_json_dict, build_direction=build_direction, table_attributes=table_attributes)
 
-print(resp)
+with open('./operators.html', 'w') as f:
+    f.write(html)
+
+print('done')
 
 
